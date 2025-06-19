b1cf584c1159eac9dfa4af740d024a4763f49880
Alessandro Mengoli <alessandro.mengoli9@gmail.com>
2025-06-20 00:18:10 +0200

    init sk without DI

--- 20250627_workingsoftware/base_sk/Console_Base_Sk/Console_Base_Sk/AIService.cs
+++ 20250627_workingsoftware/base_sk/Console_Base_Sk/Console_Base_Sk/AIService.cs
@@ -23,7 +23,7 @@ class BaseAIService(Kernel kernel) : IAIService
 
          
          ChatOptions options = new ChatOptions();
-         IChatCompletionService chatService = kernel.GetRequiredService<IChatCompletionService>("azure");
+         IChatCompletionService chatService = kernel.GetRequiredService<IChatCompletionService>("llama");
          bool roleWritten = false;
          string fullMessage = string.Empty;
          List<StreamingChatMessageContent> chatUpdates = [];
@@ -43,29 +43,39 @@ class BaseAIService(Kernel kernel) : IAIService
 
 }
 
-class PluginAIService(Kernel kernel) : IAIService
+
+class NoDIAIService(Kernel kernel1) : IAIService
 {
 
-   public async Task<string> GetResponseAsyncNo(string prompt, ChatHistory history)
+   public async Task<string> GetResponseAsync(string prompt, ChatHistory history)
    {
-      var settings = new OllamaPromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+      
+      var modelId = "gpt-4.1";
+      var endpoint = "https://austinsvedese.openai.azure.com/";
+      //var apiKey = "3a7534d534354bfe8f686a2ba53a08ba";
+      var apiKey = "3a7534d534354bfe8f686a2ba53a08ba";
+      var handler = new HttpClientHandler();
+      handler.CheckCertificateRevocationList = false;
+      var client = new HttpClient(handler);
+      var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey, httpClient: client,
+        serviceId: "azure");
 
-      var chatService = kernel.GetRequiredService<IChatCompletionService>();
-      //kernel.Plugins.AddFromType<TimePlugin>("Text");
-      kernel.Plugins
-    .AddFromObject(new TimePlugin());
-      history.AddUserMessage(prompt);
+      Kernel kernel2 = builder.Build();
+      using var activity = ApplicationDiagnostics.ActivitySource.StartActivity("GetResponse");
+      OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+      {
+         FunctionChoiceBehavior = FunctionChoiceBehavior.None()
+      };
+      kernel2.Plugins.AddFromType<TimePlugin>("Text");
+
       ChatOptions options = new ChatOptions();
-
+      IChatCompletionService chatService = kernel2.GetRequiredService<IChatCompletionService>();
       bool roleWritten = false;
       string fullMessage = string.Empty;
       List<StreamingChatMessageContent> chatUpdates = [];
-
-      Microsoft.SemanticKernel.ChatMessageContent chatResult = await chatService.GetChatMessageContentAsync(prompt, settings, kernel);
-
       await foreach (var chatUpdate in chatService.GetStreamingChatMessageContentsAsync(history,
-         executionSettings: settings,
-   kernel: kernel))
+         executionSettings: openAIPromptExecutionSettings,
+   kernel: kernel2))
       {
          chatUpdates.Add(chatUpdate);
 
@@ -75,6 +85,44 @@ class PluginAIService(Kernel kernel) : IAIService
       Console.WriteLine("\n------------------------");
 
       return chatUpdates.ToString();
+   }
+
+}
+
+
+class PluginAIService(Kernel kernel) : IAIService
+{
+
+   public async Task<string> GetResponseAsyncNo(string prompt, ChatHistory history)
+   {
+      var settings = new OllamaPromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+      var chatService = kernel.GetRequiredService<IChatCompletionService>();
+      //kernel.Plugins.AddFromType<TimePlugin>("Text");
+
+      history.AddUserMessage(prompt);
+      ChatOptions options = new ChatOptions();
+
+      bool roleWritten = false;
+      string fullMessage = string.Empty;
+      List<StreamingChatMessageContent> chatUpdates = [];
+
+      Microsoft.SemanticKernel.ChatMessageContent chatResult = await chatService.GetChatMessageContentAsync(prompt, settings, kernel);
+
+   //   await foreach (var chatUpdate in chatService.GetStreamingChatMessageContentsAsync(history,
+   //      executionSettings: settings,
+   //kernel: kernel))
+   //   {
+   //      chatUpdates.Add(chatUpdate);
+
+   //      Console.Write(chatUpdate);
+   //   }
+
+      var result = await chatService.GetChatMessageContentAsync(history, executionSettings: settings, kernel: kernel);
+      Console.WriteLine(result);
+      Console.WriteLine("\n------------------------");
+
+      return result.ToString();
    }
    public async Task<string> GetResponseAsync(string prompt, ChatHistory history)
    {
@@ -96,18 +144,20 @@ class PluginAIService(Kernel kernel) : IAIService
       bool roleWritten = false;
       string fullMessage = string.Empty;
       List<StreamingChatMessageContent> chatUpdates = [];
-      await foreach (var chatUpdate in chatService.GetStreamingChatMessageContentsAsync(history,
-         executionSettings: settings,
-   kernel: kernel))
-      {
-         chatUpdates.Add(chatUpdate);
+      //   await foreach (var chatUpdate in chatService.GetStreamingChatMessageContentsAsync(history,
+      //      executionSettings: settings,
+      //kernel: kernel))
+      //   {
+      //      chatUpdates.Add(chatUpdate);
 
-         Console.Write(chatUpdate);
-      }
+      //      Console.Write(chatUpdate);
+      //   }
 
+      var result = await chatService.GetChatMessageContentAsync(history, executionSettings: settings, kernel: kernel);
+      Console.WriteLine(result);
       Console.WriteLine("\n------------------------");
 
-      return chatUpdates.ToString();
+      return result.ToString();
    }
 }
 
--- 20250627_workingsoftware/base_sk/Console_Base_Sk/Console_Base_Sk/AppService.cs
+++ 20250627_workingsoftware/base_sk/Console_Base_Sk/Console_Base_Sk/AppService.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Console_Base_Sk.Plugin;
+using Menu.Utilities;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
 using Microsoft.SemanticKernel.Services;
 using System;
 using System.Collections.Generic;
@@ -25,7 +29,7 @@ public class AppService(IAIService aiService, ILogger<AppService> logger, IHostA
          Console.WriteLine(new string('=', 50));
 
          // Avvia interfaccia interattiva
-         await DemoSemanticKernel();
+         await DemoSemanticKernelok();
       }
       catch (Exception ex)
       {
@@ -43,8 +47,10 @@ public class AppService(IAIService aiService, ILogger<AppService> logger, IHostA
       return Task.CompletedTask;
    }
 
-   private async Task DemoSemanticKernel()
+   private async Task DemoSemanticKernelok()
    {
+      using var activity = ApplicationDiagnostics.ActivitySource.StartActivity("AppService");
+
       string? userInput;
       var history = new ChatHistory();
       do
@@ -66,5 +72,51 @@ public class AppService(IAIService aiService, ILogger<AppService> logger, IHostA
       } while (userInput is not null);
    }
 
+
+   private async Task DemoSemanticKernel()
+   {
+      var builder = Kernel.CreateBuilder();
+      var modelId = "llama3.2";
+      var endpoint = new Uri("http://localhost:11434");
+
+      builder.Services.AddOllamaChatCompletion(modelId, endpoint);
+
+      builder.Plugins
+          .AddFromObject(new TimePlugin());
+
+
+      Kernel kernel = builder.Build();
+      var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+      var settings = new OllamaPromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+      Console.WriteLine("""
+    Ask questions or give instructions to the copilot such as:
+    - Change the alarm to 8
+    - What is the current alarm set?
+    - Is the light on?
+    - Turn the light off please.
+    - Set an alarm for 6:00 am.
+    """);
+
+      Console.Write("> ");
+
+      string? input = null;
+      while ((input = Console.ReadLine()) is not null)
+      {
+         Console.WriteLine();
+
+         try
+         {
+            ChatMessageContent chatResult = await chatCompletionService.GetChatMessageContentAsync(input, settings, kernel);
+            Console.Write($"\n>>> Result: {chatResult}\n\n> ");
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine($"Error: {ex.Message}\n\n> ");
+         }
+      }
+
+   }
 
 }
