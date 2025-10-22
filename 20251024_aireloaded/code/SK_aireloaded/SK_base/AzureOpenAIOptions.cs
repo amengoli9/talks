a82b31cbf7ab42d0fe7cde4b6272782fdf6c4b96
Alessandro Mengoli <alessandro.mengoli9@gmail.com>
2025-10-22 22:22:28 +0200

    init aiday repo

---
+++ 20251024_aireloaded/code/SK_aireloaded/SK_base/AzureOpenAIOptions.cs
@@ -0,0 +1,6 @@
+﻿public sealed class AzureOpenAIOptions
+{
+   public string? DeploymentName { get; set; }
+   public string? Endpoint { get; set; }
+   public string? ApiKey { get; set; }
+}
---
+++ 20251024_aireloaded/code/SK_aireloaded/SK_base/Program.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+
+
+var host =  Host.CreateApplicationBuilder(args);
+using var activitySource = new System.Diagnostics.ActivitySource("ai_demo");
+
+# region handle HTTPCLIENT
+var handler = new HttpClientHandler();
+handler.CheckCertificateRevocationList = false;
+var client = new HttpClient(handler);
+HttpClient clientfaker = new HttpClient(new FakerHttpMessageHandler());
+# endregion
+
+#region OpenTelemetry setup
+AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
+var resourceBuilder = ResourceBuilder
+          .CreateDefault()
+          .AddService("AIDay_BASE");
+
+host.Services.AddOpenTelemetry()
+           .ConfigureResource(r => r.AddService("SkDemoBase", serviceVersion: "1.0.0"))
+           .WithTracing(builder =>
+           {
+              builder
+              .SetResourceBuilder(resourceBuilder)
+              .AddSource("Microsoft.Extensions.AI*")
+              .AddSource("Microsoft.SemanticKernel*")
+              .AddSource("Experimental.Microsoft.Extensions.AI.*")
+              .AddSource("ai_demo")
+              .AddHttpClientInstrumentation()
+              .AddOtlpExporter();
+           })
+           .WithMetrics(builder =>
+           {
+              builder
+              .SetResourceBuilder(resourceBuilder)
+              .AddMeter("Microsoft.SemanticKernel*")
+              .AddMeter("Microsoft.Extensions.AI*")
+              .AddMeter("Experimental.Microsoft.Extensions.AI.*")
+              .AddOtlpExporter();
+           });
+host.Logging.ClearProviders();
+host.Logging.AddOpenTelemetry(options =>
+{
+   options.SetResourceBuilder(resourceBuilder);
+   options.AddOtlpExporter(); 
+   options.IncludeFormattedMessage = true;
+   options.IncludeScopes = true;
+});
+#endregion
+var app = host.Build();
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+var tracerProvider = app.Services.GetRequiredService<TracerProvider>();
+
+#region configure AI services
+var aoai = host.Configuration.GetSection("AzureOpenAI").Get<AzureOpenAIOptions>()
+               ?? throw new InvalidOperationException("Sezione 'AzureOpenAI' mancante in configuration.");
+if (string.IsNullOrWhiteSpace(aoai.DeploymentName) ||
+        string.IsNullOrWhiteSpace(aoai.Endpoint) ||
+        string.IsNullOrWhiteSpace(aoai.ApiKey))
+{
+   throw new InvalidOperationException("Configurazione AzureOpenAI incompleta: DeploymentName/Endpoint/ApiKey.");
+}
+#endregion
+
+#region base
+//using (var activity = activitySource.StartActivity("INIT_BASE"))
+//{
+
+//   IKernelBuilder builder = Kernel.CreateBuilder();
+//builder.AddAzureOpenAIChatCompletion(aoai.DeploymentName!, aoai.Endpoint!, aoai.ApiKey!, httpClient: client);
+
+//   Kernel kernel = builder.Build();
+//   var answer = await kernel.InvokePromptAsync("Say hi");
+//   Console.WriteLine(answer);
+//}
+#endregion
+
+
+
+#region chat
+using (var activity = activitySource.StartActivity("INIT_CHAT"))
+{
+
+   IKernelBuilder builder = Kernel.CreateBuilder();
+   builder.AddAzureOpenAIChatCompletion(aoai.DeploymentName!, aoai.Endpoint!, aoai.ApiKey!,httpClient: client );
+
+   Kernel kernel = builder.Build();
+   ChatCompletionAgent chatCompletionAgent = new()
+   {
+      Name = "chat-agent",
+      Description = "An agent that can chat with the user.",
+      Kernel = kernel
+   };
+   string? userInput;
+   AgentThread thread = new ChatHistoryAgentThread();
+   do
+   {
+      Console.Write("User > ");
+      userInput = Console.ReadLine(); 
+      if (userInput is null)
+         break;
+      using var userMessageActivity = activitySource.StartActivity("UserMessage", ActivityKind.Internal);
+      userMessageActivity?.SetTag("message.length", userInput.Length);
+      var res =  chatCompletionAgent.InvokeAsync(userInput, thread);
+      StringBuilder sb = new StringBuilder();
+      await foreach (var message in res)
+      {
+         sb.Append(message.Message);
+      }
+      Console.WriteLine($"AI > {sb.ToString()}");
+
+
+   } while (userInput is not null || userInput.Trim() != "addio");
+
+}
+#endregion
+
+
+
+
+
+
+
+
+
+
+await app.RunAsync();
